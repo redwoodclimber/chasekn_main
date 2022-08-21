fc6401536566c81f9b0281ade31ffe5ea010705c
Chase Knowland <cknowland1784@sdsu.edu>
2022-08-20 19:42:37 -0700

    add tailwind and firebase

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+//import './App.css'; // Using tailwind -- App.css not included
+// React components
+import React, { useRef, useState } from 'react';
+import Navbar from 'react-bootstrap/Navbar';
+
+// Firebase (v9 modular)
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDolnvqXyWNg6Um3ZjKC2Dnr_dryr3rOig",
+  authDomain: "chasekn-main.firebaseapp.com",
+  projectId: "chasekn-main",
+  storageBucket: "chasekn-main.appspot.com",
+  messagingSenderId: "96593648457",
+  appId: "1:96593648457:web:1e7d038161e7bb5f48d898",
+  measurementId: "G-YR1FYQER9L"
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+const analytics = getAnalytics(firebaseApp);
+const db = getFirestore(firebaseApp);
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar />
+      <div className="flex flex-col items-center mt-16">
+        <h1 className="text-4xl text-center">Tailwind CSS makes styling React components easier!</h1>
+        <button className="bg-black text-white p-2.5 w-fit mt-9">Get Started</button>
+      </div>
     </div>
   );
 }
